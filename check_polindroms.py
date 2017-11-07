38f9bbb7347110ba6a496233eb54a4f1e09fe466
ermins <ermins@yahoo.co.uk>
2017-11-07 23:49:06 +0000

    Py test

---
+++ check_polindroms.py
@@ -0,0 +1,51 @@
+import math
+
+    
+final_list=[]
+
+def convert_number_to_base_x(n, b):
+    if n == 0:
+        return [0]
+    digits = []
+    while n:
+        digits.append(int(n % b))
+        n /= b
+    return digits[::-1]
+
+def build_the_number(temp_data):
+    digit_values={10:'a',11:'b',12:'c',13:'d',14:'e',15:'f',16:'g',17:'h',18:'i',19:'j',
+             20:'k',21:'l',22:'m',23:'n',24:'o',25:'p',26:'q',27:'r',28:'s',29:'t',
+             30:'u',31:'v',32:'w',33:'x',34:'y',35:'z'}
+    number = ''
+    for digit in temp_data:
+        if digit > 9:
+            number += digit_values[digit]
+        else:
+            number += str(digit)
+    return number
+
+def check_if_number_is_polindrom(test_num):
+    if test_num == test_num[::-1]:
+        return True
+    else:
+        return False
+
+def check_numbers():
+    for num in range (1,1001):
+        for base in range (2,35):
+            data =[]
+            result = None
+            t_num = convert_number_to_base_x(num, base)
+            f_num = build_the_number(t_num)
+            result = check_if_number_is_polindrom(f_num)
+            if result == True:
+                final_list.append([num,base])
+                break;
+    return final_list
+
+
+print 'Please wait'
+fl = check_numbers()
+for d in fl:
+    print d
+print 'Finished'
---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+import check_polindroms
+class TestCheckPolindrom(unittest.TestCase):
+    def test_convert_number_to_base_x(self):
+    	f_result = ''
+        result = check_polindroms.convert_number_to_base_x(17,2)
+        for s in result:
+             f_result += str(s)
+        self.assertEquals(f_result, '10001')
+
+    def test_build_the_number(self):
+        result = check_polindroms.build_the_number([6,10])
+        self.assertEquals(result, '6a')
+
+    def test_check_if_number_is_polindrom(self):
+        result = check_polindroms.check_if_number_is_polindrom('11')
+        result1 = check_polindroms.check_if_number_is_polindrom('10011')
+        self.assertEquals(result, True)
+        self.assertEquals(result1, False)
